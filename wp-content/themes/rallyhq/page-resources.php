bde16507e57222984a0e641f7889a7449ef2e89e
amandaleee <amandalee@amandalee.org>
2017-02-11 11:35:07 -0600

    updates

--- wp-content/themes/rallyhq/page-resources.php
+++ wp-content/themes/rallyhq/page-resources.php
@@ -33,11 +33,56 @@ get_header(); ?>
 			<!--todo-->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="entry-content">
-					<?php the_content();?>
+				<?php if( get_field('need_to_know_header') ): ?>
+					<h2><?php the_field('need_to_know_header'); ?></h2>
+				<?php endif; ?>
+				<?php the_content();?>
+
 				</div><!-- .entry-content -->
 			</article><!-- #post-## -->
-
+			<section class="faqs">
+				<?php
+					$loop = new WP_Query( array( 'post_type' => 'faq', 'order' => 'ASC'  ) );
+				    if ( $loop->have_posts() ) :
+			        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+									<?php echo get_the_title(); ?>
+									<?php echo the_content(); ?>
+									<!--todo-->
+			        <?php endwhile;
+				    endif;
+				    wp_reset_postdata();
+					?>
+			</section><!--/faqs-->
+			
+				<?php
+					$loop = new WP_Query( array( 'pagename' => 'resources'  ) );
+				    if ( $loop->have_posts() ) :
+			        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				      <section class="make-a-call">
+								<?php if( get_field('make_a_call_header') ): ?>
+									<h2><?php the_field('make_a_call_header'); ?></h2>
+								<?php endif; ?>
+								<?php if( get_field('make_a_call_body') ): ?>
+									<h2><?php the_field('make_a_call_body'); ?></h2>
+								<?php endif; ?>
+								</section><!--/make-a-call-->
+								<section class="call-video">
+									<?php if( get_field('how_to_call_video') ): ?>
+										<?php the_field('how_to_call_video'); ?>
+								<?php endif; ?>
+								</section><!--/call-video-->
+								<section class="pull-quote">
+									<?php if( get_field('pull_quote') ): ?>
+										<?php the_field('pull_quote'); ?>
+								<?php endif; ?>
+								</section><!--/call-video-->
+				        <?php endwhile;
+				    endif;
+				    wp_reset_postdata();
+					?>
+			
 		</main><!-- #main -->
+
 	
 
 		
