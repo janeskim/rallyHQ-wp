9c2d6b73e8da84f6f4abf95a0cc5a09586b930fd
amandaleee <amandalee@amandalee.org>
2017-02-11 11:55:51 -0600

    updating gitignore, new scss folder and stuff

--- wp-content/themes/rallyhq/functions.php
+++ wp-content/themes/rallyhq/functions.php
@@ -120,6 +120,7 @@ add_action( 'widgets_init', 'arphabet_widgets_init' );
  */
 function rallyhq_scripts() {
 	wp_enqueue_style( 'rallyhq-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'rallyhq-app-style', get_template_directory_uri() . '/app.css', array(), null, 'all' );
 
 	wp_enqueue_script( 'rallyhq-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
