cb47f49aff117763a59c5ab0c54c0433bd333ed7
amandaleee <amandalee@amandalee.org>
2017-02-11 10:54:49 -0600

    backups

--- wp-content/uploads/sucuri/sucuri-settings.php
+++ wp-content/uploads/sucuri/sucuri-settings.php
@@ -1,2 +1,2 @@
 <?php exit(0); ?>
-{"sucuriscan_plugin_version":"1.8.3","sucuriscan_emails_sent":5,"sucuriscan_last_email_at":1486786384}
+{"sucuriscan_plugin_version":"1.8.3","sucuriscan_emails_sent":5,"sucuriscan_last_email_at":1486786384,"sucuriscan_runtime":1486824569}
