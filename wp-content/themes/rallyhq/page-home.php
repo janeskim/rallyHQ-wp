4f75022a126638c61b889e45596194627323ad1e
amandaleee <amandalee@amandalee.org>
2017-02-10 21:30:19 -0600

    mailchimp form is there

--- wp-content/themes/rallyhq/page-home.php
+++ wp-content/themes/rallyhq/page-home.php
@@ -107,7 +107,39 @@ get_header(); ?>
 			?>
 		</section>
 		<div class="mailchimp" style="border:1px solid silver; height:300px;"">
-			placeholder - mailchimp signup form goes here
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+			<form action="//RallyHQ.us15.list-manage.com/subscribe/post?u=fd7e409dee67f2d831fa75b71&amp;id=485a536aec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			    <div id="mc_embed_signup_scroll">
+			    <h2>Subscribe to our mailing list</h2>
+			<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+			<div class="mc-field-group">
+			    <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+			</label>
+			    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div class="mc-field-group">
+			    <label for="mce-FNAME">First Name </label>
+			    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+			</div>
+			<div class="mc-field-group">
+			    <label for="mce-LNAME">Last Name </label>
+			    <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+			</div>
+			<div class="mc-field-group">
+			    <label for="mce-MMERGE3">Zip Code </label>
+			    <input type="text" value="" name="MMERGE3" class="" id="mce-MMERGE3">
+			</div>
+			    <div id="mce-responses" class="clear">
+			        <div class="response" id="mce-error-response" style="display:none"></div>
+			        <div class="response" id="mce-success-response" style="display:none"></div>
+			    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fd7e409dee67f2d831fa75b71_485a536aec" tabindex="-1" value=""></div>
+			    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			    </div>
+			</form>
+			</div>
+			<!--End mc_embed_signup-->
 		</div><!--/mailchimp-->
 
 		<section class="quotes">
@@ -124,30 +156,14 @@ get_header(); ?>
 
 			?>
 		</section><!--/quotes-->
-		<section class="join">
-			<?php
-		    $loop = new WP_Query( array( 'pagename' => 'home' ) );
-		    if ( $loop->have_posts() ) :
-		        while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                <?php if( get_field('join_form_header') ): ?>
-									<h2><?php the_field('join_form_header'); ?></h2>
-								<?php endif; ?>
-								<?php if( get_field('join_form_field') ): ?>
-									<h2><?php the_field('join_form_field'); ?></h2>
-								<?php endif; ?>
-		        <?php endwhile;
-		    endif;
-
-		    wp_reset_postdata();
-			?>
 		
-		</section><!--/join-->
+		
 		<section class="instagram-feed">
 			<?php if ( is_active_sidebar( 'home_widget_1' ) ) : ?>
-	<div id="" class=" widget-area" role="complementary">
-		<?php dynamic_sidebar( 'home_widget_1' ); ?>
-	</div><!-- #primary-sidebar -->
-<?php endif; ?>
+				<div id="" class=" widget-area" role="complementary">
+					<?php dynamic_sidebar( 'home_widget_1' ); ?>
+				</div><!--/widget-area -->
+			<?php endif; ?>
 		</section><!--/instagram-feed-->
 	</div><!--/#primary -->
 
