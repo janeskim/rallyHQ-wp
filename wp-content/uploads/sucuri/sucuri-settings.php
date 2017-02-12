2c75d13bee7cad1d88d93c84615a11d80bd990a8
Violet Forest <violetforest@Violet-Forest.local>
2017-02-12 07:17:40 -0600

    colors according to PSD

--- wp-content/uploads/sucuri/sucuri-settings.php
+++ wp-content/uploads/sucuri/sucuri-settings.php
@@ -1,2 +1,2 @@
 <?php exit(0); ?>
-{"sucuriscan_plugin_version":"1.8.3","sucuriscan_emails_sent":1,"sucuriscan_last_email_at":1486853866,"sucuriscan_runtime":1486853870}
+{"sucuriscan_plugin_version":"1.8.3","sucuriscan_emails_sent":1,"sucuriscan_last_email_at":1486853866,"sucuriscan_runtime":1486905202}
