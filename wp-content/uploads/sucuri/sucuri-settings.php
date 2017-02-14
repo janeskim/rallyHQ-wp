85f0b2708960b025be68d657e85fdc8f55c7b352
Violet Forest <violetforest@Violet-Forest.local>
2017-02-14 08:01:40 -0600

    backup with updraft

--- wp-content/uploads/sucuri/sucuri-settings.php
+++ wp-content/uploads/sucuri/sucuri-settings.php
@@ -1,2 +1,2 @@
 <?php exit(0); ?>
-{"sucuriscan_plugin_version":"1.8.3","sucuriscan_emails_sent":2,"sucuriscan_last_email_at":1486907759,"sucuriscan_runtime":1486905202}
+{"sucuriscan_plugin_version":"1.8.3","sucuriscan_emails_sent":2,"sucuriscan_last_email_at":1486907759,"sucuriscan_runtime":1487080780}
