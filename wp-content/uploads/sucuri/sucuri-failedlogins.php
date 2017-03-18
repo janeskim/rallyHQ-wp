838aac1929f2833b8b0a83d8e8929f922b12e989
Violet Forest <violetforest@Violet-Forest.local>
2017-03-18 15:02:58 -0500

    backup try agian

---
+++ wp-content/uploads/sucuri/sucuri-failedlogins.php
@@ -0,0 +1,2 @@
+<?php exit(0); ?>
+{"user_login":"violetforest","user_password":"","attempt_time":1489867077,"remote_addr":"127.0.0.1","user_agent":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/56.0.2924.87 Safari\/537.36"}
--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -1,2 +1,3 @@
 <?php exit(0); ?>
 {"user_id":2,"user_login":"developer","user_remoteaddr":"127.0.0.1","user_hostname":"localhost","user_lastlogin":"2017-02-11 22:57:46"}
+{"user_id":2,"user_login":"developer","user_remoteaddr":"127.0.0.1","user_hostname":"localhost","user_lastlogin":"2017-03-18 19:58:37"}
