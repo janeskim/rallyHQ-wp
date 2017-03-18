d0f765437141f7d572ed616d0492841eb21c0adc
Violet Forest <violetforest@Violet-Forest.local>
2017-03-18 17:16:58 -0500

    lato reg

--- wp-content/themes/rallyhq/header.php
+++ wp-content/themes/rallyhq/header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link href="https://fonts.googleapis.com/css?family=Playfair+Display:900" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
 
 <?php wp_head(); ?>
